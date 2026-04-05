a3520af323170e70c98645e260926359b95f4b59
saudagarmadiha19-ctrl <saudagarmadiha19@gmail.com>
2026-04-05 15:16:54 +0530

    Create CaseConvert.java

---
+++ Basics/CaseConvert.java
@@ -0,0 +1,10 @@
+/*
+Convert uppercase to lowercase
+*/
+
+class CaseConvert {
+    public static void main(String[] args) {
+        String str = "HELLO";
+        System.out.println(str.toLowerCase());
+    }
+}
