41bd56e70a3bf44eeffa4a7bce41607b37c5e0e5
saudagarmadiha19-ctrl <saudagarmadiha19@gmail.com>
2026-04-05 14:24:40 +0530

    Add Student class with display method

---
+++ OOP/ClassAndObject.java
@@ -0,0 +1,17 @@
+class Student 
+{
+  String name;
+  int age;
+  void display() 
+  {
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+  }
+  public static void main(String[] args) 
+  {
+    Student s1 = new Student();
+    s1.name = "Priya";
+    s1.age = 20;
+    s1.display();
+  }
+}
