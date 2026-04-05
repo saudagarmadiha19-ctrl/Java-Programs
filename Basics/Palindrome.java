3a869f182cae81251caea9d95a5f650d60b3405e
saudagarmadiha19-ctrl <saudagarmadiha19@gmail.com>
2026-04-05 15:01:25 +0530

    Create Palindrome.java

---
+++ Basics/Palindrome.java
@@ -0,0 +1,18 @@
+/*
+Palindrome Number
+*/
+
+class Palindrome {
+    public static void main(String[] args) {
+        int num = 121, rev = 0, temp = num;
+        while(num != 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num /= 10;
+        }
+        if(temp == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
