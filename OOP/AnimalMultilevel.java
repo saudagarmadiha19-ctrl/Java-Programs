efd937e679fb0a0610d8f894268e2100c7c9c434
saudagarmadiha19-ctrl <saudagarmadiha19@gmail.com>
2026-04-05 14:50:16 +0530

    Create AnimalMultilevel.java

---
+++ OOP/AnimalMultilevel.java
@@ -0,0 +1,21 @@
+/*
+Concept: Multilevel Inheritance
+*/
+
+class AnimalMultilevel {
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+class Puppy extends Dog {
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+        p.eat();
+        p.bark();
+    }
+}
