cc45a328dc7ef271fdc2c38b5ddb53133928633d
saudagarmadiha19-ctrl <saudagarmadiha19@gmail.com>
2026-04-05 15:08:12 +0530

    Create EvenOdd.java

---
+++ Basics/EvenOdd.java
@@ -0,0 +1,13 @@
+/*
+Check Even or Odd
+*/
+
+class EvenOdd {
+    public static void main(String[] args) {
+        int num = 10;
+        if(num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
